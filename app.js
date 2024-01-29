df716d2c922139cfa3125930b840718af8373aac
shafee_azeem <shafeeazeemag@gmail.com>
2024-01-29 07:39:07 +0530

    asynchronous programming

--- app.js
+++ app.js
@@ -4,64 +4,64 @@ console.log(1);
 console.log(2);
 console.log(3);
 
-
 //ASYNC run parrallel without stopping all the instruction
 
-
-function hello(){
-    console.log("sup my sister"); 
+function hello() {
+  console.log("sup my sister");
 }
 
-setTimeout(hello,2000);
+setTimeout(hello, 2000);
 
 ///
 
-setTimeout(()=>{
-console.log("Hello my brother"); 
+setTimeout(() => {
+  console.log("Hello my brother");
+}, 8000);
 
-},8000); 
-
-
+function myname() {
+  console.log("my name is sheila.");
+}
+myname();
 console.log(4);
 console.log(5);
 
-function sum(a,b){
-    console.log(a+b); 
+function sum(a, b) {
+  console.log(a + b);
 }
 
-function calc(a,b, sumCum){
-    sumCum(a,b);
+function calc(a, b, sumCum) {
+  sumCum(a, b);
 }
 
-calc(1,2,sum);
+calc(1, 2, sum);
 
 //
 
 // var randomNumber = Math.floor(Math.random() * 100);
 // // alert("Random number generated is "+ randomNumber);
 
-// var Geusscount=0; 
+// var Geusscount=0;
 
 // function game(){
 
-//     let UserInput = parseInt(prompt("Enter your number: "));  
+//     let UserInput = parseInt(prompt("Enter your number: "));
 
 //     if(UserInput<=0 || UserInput>=100){
-//         alert("the number must be betweeen 1-100"); 
+//         alert("the number must be betweeen 1-100");
 //     }
 //     else if (randomNumber===UserInput){
 //         alert("Your guess is right");
-//         Geusscount++; 
-//         game(); 
+//         Geusscount++;
+//         game();
 //     }else if (UserInput>randomNumber){
 //         alert("go lower");
-//         Geusscount++; 
-//         game(); 
+//         Geusscount++;
+//         game();
 //     }else if  (UserInput<randomNumber){
 //         alert("go higher");
-//         Geusscount++;  
+//         Geusscount++;
 //         game();
 //     }
 // }
 
-// game(); 
+// game();
--- app2.js
+++ app2.js
@@ -1,15 +1,81 @@
-function getdata(data, getNextData, getNexTestData)
-{
-    setTimeout( ()=>{
-        console.log("the data is", data);
-        if(getNextData){
-            getNextData();
-        }
-        if (getNexTestData){
-            getNexTestData();
-        }
-    },4000); 
-        
+function getdata(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data);
+    }, 1000);
+  });
 }
 
-getdata(3, ()=>{getdata(2)},()=>{getdata(4)} ); 
+getdata(1)
+  .then((res) => {
+    console.log(res);
+
+    return getdata(2);
+  })
+  .then((res) => {
+    console.log(res);
+
+    return getdata(3);
+  })
+  .then((res) => {
+    console.log(res);
+
+    return getdata(4);
+  })
+  .then((res) => {
+    console.log(res);
+
+    return getdata(5);
+  })
+  .then((res) => {
+    console.log(res);
+    return getdata(6);
+  })
+  .then((res) => {
+    console.log(res);
+
+    console.log("all data's fecthed");
+  });
+
+// function asyncFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("fecthing data 1");
+//       resolve("success");
+//     }, 2000);
+//   });
+// }
+
+// function asyncFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("  fetching data 2...");
+//       resolve("success");
+//     }, 2000);
+//   });
+// }
+
+// function asyncFunc3() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("  fetching data 3...");
+//       resolve("success");
+//     }, 2000);
+//   });
+// }
+
+// asyncFunc1().then((res) => {
+//   console.log(res);
+//   asyncFunc2().then((res) => {
+//     console.log(res);
+//     asyncFunc3().then((res) => {
+//       console.log(res);
+//     });
+//   });
+// });
+
+// console.log("fecthing data 2...");
+// let P2 = asyncFunc2();
+// P2.then((res) => {
+//   console.log(res);
+// })
